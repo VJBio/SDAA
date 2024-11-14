4ee10d6f03832ebac23fe6e1d542638ac3bc51a6
VJBio <Vineet.Jha@pfizer.com>
2024-11-14 13:54:38 +0000

    plot update

--- modules/Data_Insights3.R
+++ modules/Data_Insights3.R
@@ -239,7 +239,26 @@ Data_Insights_server_3 <- function(id, uploadedData) {
         layout(dragmode = "select")
       event_register(p, "plotly_click")  # Register plotly click event
       
-      p
+      hline <- function(y = 0, color = "black") {
+        list(
+          type = "line",
+          x0 = 0,
+          x1 = 1,
+          xref = "paper",
+          y0 = y,
+          y1 = y,
+          line = list(color = color)
+        )
+      }
+      library(plotly)
+      fig <- plot_ly( filtered , y = ~PCORRES_numeric, color = ~PCTPT_factor ,  type = "box")  %>%
+               layout(shapes = list(hline(900 , color="red") ) )  %>%
+                layout(shapes = list(hline(300 , color="red") ) )
+      
+      
+      fig
+      
+      #p
     })
     
     # Initially rendering an empty data table
